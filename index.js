403696583fb6acc1dbf8e55374e6a618b05aab76
WENDEL GABRIEL HANG <wendel_hang@estudante.sesisenai.org.br>
2024-07-04 16:31:18 -0300

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-function filtrarPares(arr){
+const express = require('express')
+
+const app = express()
+
+app.get('/test-api' , function (req, res)  {
+    res.send(' NOSSA API TA FUNCIONANDO')
+})
+
+app.listen(8000)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /*function filtrarPares(arr){
     nova_arr = []
     for(let i=0; i < arr.length; i++){
         if (arr[i] % 2 === 0) {
@@ -10,5 +37,5 @@ function filtrarPares(arr){
 } 
  arr = [1,2,3,4,5,6]
  resultado = filtrarPares(arr)
- console.log(resultado)   
+ console.log(resultado)   */ 
 
